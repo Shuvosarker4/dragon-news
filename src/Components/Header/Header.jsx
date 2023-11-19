40ecb39743369c486f2ac63b10bb43106775491b
Shuvosarker4 <shuvo01797451@gmail.com>
2023-11-19 15:01:17 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import moment from "moment/moment";
+import logo from "../../../public/logo.png";
+import Marquee from "react-fast-marquee";
+
+const Header = () => {
+  return (
+    <div>
+      <div
+        className=" flex flex-col space-y-3
+
+         justify-center items-center
+      "
+      >
+        <img src={logo} alt="" />
+        <p>Journalism Without Fear or Favour</p>
+        <h1 className="text-xl font-medium">
+          {moment().format("dddd, MMMM D, YYYY")}
+        </h1>
+      </div>
+      <div
+        className="flex justify-center
+      items-center
+      "
+      >
+        <button className="btn btn-secondary">Latest</button>
+        <div>
+          <Marquee pauseOnHover={true}>
+            Match Highlights: India vs Australia — as it happened ! Match
+            Highlights: India vs Australia as...
+          </Marquee>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../Components/Header/Header";
+import Navbar from "../../Components/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Home;
